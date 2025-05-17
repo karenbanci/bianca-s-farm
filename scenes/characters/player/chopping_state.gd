cd4fc3acf9eca98cd1d26304dd5a3be3c818167a
Karen <karenbanci1@gmail.com>
2025-05-16 23:23:08 -0700

    Create damage on trees

--- scenes/characters/player/chopping_state.gd
+++ scenes/characters/player/chopping_state.gd
@@ -2,6 +2,12 @@ extends NodeState
 
 @export var player: Player
 @export var animated_sprite_2D: AnimatedSprite2D
+@export var hit_component_collision_shape : CollisionShape2D
+
+func _ready() -> void:
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0,0);
+	
 
 
 func _on_process(_delta : float) -> void:
@@ -20,15 +26,23 @@ func _on_next_transitions() -> void:
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
 		animated_sprite_2D.play("chopping_back")
+		hit_component_collision_shape.position = Vector2(0,-18)
 	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2D.play("chopping_right")
+		hit_component_collision_shape.position = Vector2(9,0)
 	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2D.play("chopping_front")
+		hit_component_collision_shape.position = Vector2(0,3)
 	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2D.play("chopping_left")
+		hit_component_collision_shape.position = Vector2(-9,0)
 	else:
 		animated_sprite_2D.play("chopping_front")
+		hit_component_collision_shape.position = Vector2(0,3)
+		
+	hit_component_collision_shape.disabled = false
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2D.stop()
+	hit_component_collision_shape.disabled = true
