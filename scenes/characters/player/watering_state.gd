583d61ee0a91f1d8194b4439ae70ff97fc79ba4f
Karen <karenbanci1@gmail.com>
2025-06-25 00:13:04 -0700

    Add watering particles and crop scenes; implement growth states enum
    - Created a new scene for watering particles with a particle material configuration.
    - Added a test scene for crops, including corn and tomato instances, and integrated a day/night cycle component.
    - Updated the game day and night cycle scene to hide the DayNightCycleComponent.
    - Enhanced the tilemap layer sorting scene by adding crops (corn and tomato) as child nodes.
    - Introduced a new enum for growth states in the data types script to manage plant growth stages.

--- scenes/characters/player/watering_state.gd
+++ scenes/characters/player/watering_state.gd
@@ -1,7 +1,12 @@
 extends NodeState
 
 @export var player: Player
-@export var animated_sprite_2D: AnimatedSprite2D
+@export var animated_sprite_2d: AnimatedSprite2D
+@export var hit_component_collision_shape: CollisionShape2D
+
+func _ready() -> void:
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0, 0);
 
 
 func _on_process(_delta : float) -> void:
@@ -13,22 +18,29 @@ func _on_physics_process(_delta : float) -> void:
 
 
 func _on_next_transitions() -> void:
-	if !animated_sprite_2D.is_playing():
+	if !animated_sprite_2d.is_playing():
 		transition.emit("Idle")
 
 
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
-		animated_sprite_2D.play("watering_back")
+		animated_sprite_2d.play("watering_back")
+		hit_component_collision_shape.position = Vector2(0, -18)
 	elif player.player_direction == Vector2.RIGHT:
-		animated_sprite_2D.play("watering_right")
+		animated_sprite_2d.play("watering_right")
+		hit_component_collision_shape.position = Vector2(9, 0)
 	elif player.player_direction == Vector2.DOWN:
-		animated_sprite_2D.play("watering_front")
+		animated_sprite_2d.play("watering_front")
+		hit_component_collision_shape.position = Vector2(0, 3)
 	elif player.player_direction == Vector2.LEFT:
-		animated_sprite_2D.play("watering_left")
+		animated_sprite_2d.play("watering_left")
+		hit_component_collision_shape.position = Vector2(-9, 0)
 	else:
-		animated_sprite_2D.play("watering_front")
-
+		animated_sprite_2d.play("watering_front")
+		hit_component_collision_shape.position = Vector2(0, 3)
+	
+	hit_component_collision_shape.disabled = false
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
+	hit_component_collision_shape.disabled = true
